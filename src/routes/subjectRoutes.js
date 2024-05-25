11313fb6619998beefefe0956789f140eca4596d
Truc Tran <tracytrle@gmail.com>
2024-05-24 22:58:53 -0500

    done with Subject Model

--- src/routes/subjectRoutes.js
+++ src/routes/subjectRoutes.js
@@ -24,12 +24,79 @@ router.post("/subjects/bulk", async (req, res) => {
 });
 
 // get all subjects
+// router.get("/subjects", async (req, res) => {
+//   try {
+//     const subjects = await Subject.findAll();
+//     res.status(200).json(subjects);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
+
 router.get("/subjects", async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize) || 10;
+  const offset = (page - 1) * pageSize;
   try {
-    const subjects = await Subject.findAll();
-    res.status(200).json(subjects);
+    const { count, rows } = await Subject.findAndCountAll({
+      limit: pageSize,
+      offset: offset,
+    });
+
+    res.status(200).json({
+      totalItems: count,
+      totalPages: Math.ceil(count / pageSize),
+      currentPage: page,
+      pageSize: pageSize,
+      subjects: rows,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Get a single subject with id
+router.get("/subjects/:id", async (req, res) => {
+  try {
+    const subject = await Subject.findByPk(req.params.id);
+    if (subject) {
+      res.status(200).json(subject);
+    } else {
+      res.status(400).json({ error: "subject not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// delete a class
+router.delete("/subjects/:id", async (req, res) => {
+  try {
+    const subject = await Subject.findByPk(req.params.id);
+    if (subject) {
+      await subject.destroy();
+      res.status(200).json(subject);
+    } else {
+      res.status(400).json({ error: "subject not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// update a subject
+router.put("/subjects/:id", async (req, res) => {
+  try {
+    const subject = await Subject.findByPk(req.params.id);
+    if (subject) {
+      await subject.update(req.body);
+      res.status(200).json(subject);
+    } else {
+      res.status(404).json({ error: "subject not found" });
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
+
 module.exports = router;
